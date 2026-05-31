d9ef3de214375a158f8e67057337f6b96ef1abdc
Sembremos <sembremoseguridad@gmail.com>
2026-05-31 11:04:30 -0600

    Update tablas.py

--- styles/tablas.py
+++ styles/tablas.py
@@ -7,6 +7,9 @@
 TABLA_L1 = {
     "ancho": 500,
     "color_header": "#31a908",
+    "color_texto_header": "#FFFFFF",
+    "color_borde": "#787878",
+    "alto_fila": 22,
     "fuente_header": 15,
     "fuente_texto": 12,
     "fuente_titulo": 15
@@ -15,6 +18,9 @@ TABLA_L1 = {
 TABLA_L2 = {
     "ancho": 500,
     "color_header": "#0e2f50",
+    "color_texto_header": "#FFFFFF",
+    "color_borde": "#787878",
+    "alto_fila": 22,
     "fuente_header": 15,
     "fuente_texto": 12,
     "fuente_titulo": 15
@@ -24,6 +30,9 @@ TABLA_L3 = {
     "ancho": 500,
     "color_header": "#31a908",
     "color_primera_columna": "#b7f4a2",
+    "color_texto_header": "#FFFFFF",
+    "color_borde": "#787878",
+    "alto_fila": 22,
     "fuente_header": 15,
     "fuente_texto": 12,
     "fuente_titulo": 15
@@ -37,6 +46,9 @@ TABLA_L3 = {
 TABLA_M = {
     "ancho": 220,
     "color_header": "#31a908",
+    "color_texto_header": "#FFFFFF",
+    "color_borde": "#787878",
+    "alto_fila": 22,
     "fuente_header": 15,
     "fuente_texto": 12,
     "fuente_titulo": 15
