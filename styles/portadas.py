86771a641ebb582845b9005487ea165a0d4ddabb
Sembremos <sembremoseguridad@gmail.com>
2026-05-30 18:39:11 -0600

    Create portadas.py

---
+++ styles/portadas.py
@@ -0,0 +1,12 @@
+from reportlab.lib.pagesizes import A4
+
+
+ANCHO_PAGINA, ALTO_PAGINA = A4
+
+
+PORTADA_COMPLETA = {
+    "x": 0,
+    "y": 0,
+    "ancho": ANCHO_PAGINA,
+    "alto": ALTO_PAGINA
+}
