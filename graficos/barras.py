d43e266fb06a60feafec6b33d2e273f5c13ff6b7
Sembremos <sembremoseguridad@gmail.com>
2026-05-31 11:05:30 -0600

    Update barras.py

--- graficos/barras.py
+++ graficos/barras.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import matplotlib.pyplot as plt
 
 from styles.graficos import (
@@ -5,62 +7,102 @@ from styles.graficos import (
 )
 
 
-def crear_grafico_barras(
+def insertar_grafico_barras_l(
+    pdf,
     categorias,
     valores,
     titulo,
-    ruta_salida
+    x,
+    y
 ):
 
     fig, ax = plt.subplots(
+
         figsize=(
+
             GRAFICO_BARRAS_L["ancho"] / 100,
+
             GRAFICO_BARRAS_L["alto"] / 100
+
         )
+
     )
 
-    colores = GRAFICO_BARRAS_L["colores"]
-
     barras = ax.bar(
+
         categorias,
+
         valores,
-        color=colores[:len(categorias)]
+
+        color=GRAFICO_BARRAS_L["colores"][
+            :len(categorias)
+        ]
+
     )
 
     ax.set_title(
+
         titulo,
-        fontsize=GRAFICO_BARRAS_L["fuente_titulo"]
-    )
 
-    ax.tick_params(
-        axis="x",
-        labelsize=GRAFICO_BARRAS_L["fuente_etiquetas"]
-    )
+        fontsize=GRAFICO_BARRAS_L[
+            "fuente_titulo"
+        ]
 
-    ax.tick_params(
-        axis="y",
-        labelsize=GRAFICO_BARRAS_L["fuente_texto"]
     )
 
     for barra, valor in zip(
+
         barras,
+
         valores
+
     ):
 
         ax.text(
+
             barra.get_x()
             + barra.get_width() / 2,
+
             barra.get_height(),
+
             f"{valor:.2f}%",
+
             ha="center"
+
         )
+
+    temp = tempfile.NamedTemporaryFile(
+
+        suffix=".png",
+
+        delete=False
+
+    )
 
     plt.tight_layout()
 
     plt.savefig(
-        ruta_salida,
-        bbox_inches="tight",
-        dpi=300
+
+        temp.name,
+
+        dpi=300,
+
+        bbox_inches="tight"
+
     )
 
     plt.close()
+
+    pdf.drawImage(
+
+        temp.name,
+
+        x,
+
+        y,
+
+        width=GRAFICO_BARRAS_L["ancho"],
+
+        height=GRAFICO_BARRAS_L["alto"]
+
+    )
