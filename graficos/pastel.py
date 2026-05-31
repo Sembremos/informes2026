6fc0ec181ed513262608d1874ef6bb53bfde6d4e
Sembremos <sembremoseguridad@gmail.com>
2026-05-31 11:05:41 -0600

    Update pastel.py

--- graficos/pastel.py
+++ graficos/pastel.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import matplotlib.pyplot as plt
 
 from styles.graficos import (
@@ -5,38 +7,83 @@ from styles.graficos import (
 )
 
 
-def crear_grafico_pastel(
+def insertar_grafico_pastel_s(
+    pdf,
     etiquetas,
     valores,
     titulo,
-    ruta_salida
+    x,
+    y
 ):
 
     fig, ax = plt.subplots(
+
         figsize=(
+
             GRAFICO_PASTEL_S["ancho"] / 100,
+
             GRAFICO_PASTEL_S["alto"] / 100
+
         )
+
     )
 
     ax.pie(
+
         valores,
+
         labels=etiquetas,
+
         autopct="%1.0f%%",
-        colors=GRAFICO_PASTEL_S["colores"]
+
+        colors=GRAFICO_PASTEL_S[
+            "colores"
+        ]
+
     )
 
     ax.set_title(
+
         titulo,
-        fontsize=GRAFICO_PASTEL_S["fuente_titulo"]
+
+        fontsize=GRAFICO_PASTEL_S[
+            "fuente_titulo"
+        ]
+
+    )
+
+    temp = tempfile.NamedTemporaryFile(
+
+        suffix=".png",
+
+        delete=False
+
     )
 
     plt.tight_layout()
 
     plt.savefig(
-        ruta_salida,
-        bbox_inches="tight",
-        dpi=300
+
+        temp.name,
+
+        dpi=300,
+
+        bbox_inches="tight"
+
     )
 
     plt.close()
+
+    pdf.drawImage(
+
+        temp.name,
+
+        x,
+
+        y,
+
+        width=GRAFICO_PASTEL_S["ancho"],
+
+        height=GRAFICO_PASTEL_S["alto"]
+
+    )
