c6fbf0530867183c09bc8b0abd3ff15a588801e1
Sembremos <sembremoseguridad@gmail.com>
2026-05-30 18:12:00 -0600

    Create graficos.py

---
+++ styles/graficos.py
@@ -0,0 +1,109 @@
+# styles/graficos.py
+
+# ==========================
+# GRÁFICOS DE BARRAS
+# ==========================
+
+GRAFICO_BARRAS_L = {
+    "ancho": 500,
+    "alto": 280,
+    "colores": [
+        "#0e2f50",
+        "#31a908",
+        "#787878",
+        "#62d939",
+        "#116bc5"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+GRAFICO_BARRAS_M = {
+    "ancho": 350,
+    "alto": 220,
+    "colores": [
+        "#31a908"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+GRAFICO_BARRAS_S = {
+    "ancho": 220,
+    "alto": 160,
+    "colores": [
+        "#31a908"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+
+# ==========================
+# GRÁFICOS DE PASTEL
+# ==========================
+
+GRAFICO_PASTEL_L = {
+    "ancho": 500,
+    "alto": 280,
+    "colores": [
+        "#0e2f50",
+        "#31a908",
+        "#787878",
+        "#62d939",
+        "#116bc5"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+GRAFICO_PASTEL_M = {
+    "ancho": 350,
+    "alto": 220,
+    "colores": [
+        "#0e2f50",
+        "#31a908",
+        "#787878",
+        "#62d939",
+        "#116bc5"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+GRAFICO_PASTEL_S = {
+    "ancho": 220,
+    "alto": 160,
+    "colores": [
+        "#0e2f50",
+        "#31a908",
+        "#787878",
+        "#62d939",
+        "#116bc5"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+
+# ==========================
+# GRÁFICOS DE LÍNEA
+# ==========================
+
+GRAFICO_LINEA = {
+    "ancho": 500,
+    "alto": 280,
+    "colores": [
+        "#0e2f50",
+        "#31a908"
+    ],
+    "fuente_etiquetas": 12,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
