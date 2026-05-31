23f48151450e4aac977ee30ffe8f44c2630fe03d
Sembremos <sembremoseguridad@gmail.com>
2026-05-30 18:20:32 -0600

    Create tablas.py

---
+++ styles/tablas.py
@@ -0,0 +1,43 @@
+# styles/tablas.py
+
+# ==========================
+# TABLAS GRANDES
+# ==========================
+
+TABLA_L1 = {
+    "ancho": 500,
+    "color_header": "#31a908",
+    "fuente_header": 15,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+TABLA_L2 = {
+    "ancho": 500,
+    "color_header": "#0e2f50",
+    "fuente_header": 15,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+TABLA_L3 = {
+    "ancho": 500,
+    "color_header": "#31a908",
+    "color_primera_columna": "#b7f4a2",
+    "fuente_header": 15,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
+
+
+# ==========================
+# TABLAS MEDIANAS
+# ==========================
+
+TABLA_M = {
+    "ancho": 220,
+    "color_header": "#31a908",
+    "fuente_header": 15,
+    "fuente_texto": 12,
+    "fuente_titulo": 15
+}
